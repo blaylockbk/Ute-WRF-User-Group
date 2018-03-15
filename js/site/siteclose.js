153cd9e7db86d33796cba3b143a52db310146a02
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-03-15 11:18:39 -0600

    update fontawesome version

--- js/site/siteclose.js
+++ js/site/siteclose.js
@@ -2,10 +2,10 @@ document.write('\
 <br>\
 <div id="footer" style="border-top:1px solid black; margin-top:-10px">\
 <p style="text-align:center; font-family:Segoe UI">\
-This page is maintained by Brian Blaylock &copy 2017, \
+This page is maintained by Brian Blaylock &copy 2018, \
  <a href="http://www.atmos.utah.edu/" style="color:#990000; text-decoration:none">Department of Atmospheric Science, University of Utah</a>. \
  <a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:brian.blaylock@utah.edu">\
-<big><i class="fa fa-envelope fa-fw" aria-hidden="true"></i></big> brian.blaylock@utah.edu\
+<big><i class="fa fa-envelope fa-fw"></i></big> brian.blaylock@utah.edu\
 </a>\
 </p>\
 </div>\
